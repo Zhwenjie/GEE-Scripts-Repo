36637af5b8bb2176311a0a741153803a27827718
Wenjie Zhang <zwj@lreis.ac.cn>
2020-06-26 22:43:47 +0800

    Create StringReplaceall.js

---
+++ QandA/StringReplaceall.js
@@ -0,0 +1,13 @@
+/*
+ 2.qq user Beimingyouyu , string replacement.
+ Note that set the last argu as global, it would conduct on all strings.
+*/
+var string1 = ee.String('I am a big fun of GEE');
+var string2 = string1.replace('a','A','g');
+print(string2)
+var string3 = ee.List(['a','b','a']);
+var string4 = string3.replaceAll('a','A');
+print(string4)
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
