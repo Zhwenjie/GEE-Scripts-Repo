f8270bd929fc3663a7f89acfdb40966d1640673d
Wenjie Zhang <zwj@lreis.ac.cn>
2020-07-22 17:46:31 +0800

    Update resample_bicubic_from30mto500m.js

--- QandA/resample_bicubic_from30mto500m.js
+++ QandA/resample_bicubic_from30mto500m.js
@@ -1,6 +1,7 @@
 /* 
 resample from landsat 30m resolution to 500m. This is useful when comparing image datasets at different scales, 
 for example 30-meter pixels from a Landsat-based product to coarse pixels (higher scale) from a MODIS-based product.
+Please keep an eye on the comparison between resample method and average aggregation
 reference link:https://gis.stackexchange.com/questions/316596/how-can-i-resample-a-image-or-imagecollection-to-a-higher-resolution-in-google-e
 */
 
@@ -23,6 +24,42 @@ var resampled2 = landsat.resample('bicubic').reproject({
 'crs': crs,
 'scale': 500.0});
 
+// Load a MODIS EVI image.
+var modis = ee.Image(ee.ImageCollection('MODIS/006/MOD13A1').first())
+    .select('EVI');
+
+// Display the EVI image near La Honda, California.
+Map.setCenter(-122.3616, 37.5331, 12);
+Map.addLayer(modis, {min: 2000, max: 5000}, 'MODIS EVI');
+
+
 // Display the Landsat image using bicubic resampling.
 Map.addLayer(resampled1, visParams, 'nearest neighbor resampled');
 Map.addLayer(resampled2, visParams, 'cubic resampled');
+
+
+///////////////////////////**************************************////////////////////////////
+///////--------------------------comparison with average aggregation----------------------
+// Get information about the MODIS projection.
+var modisProjection = modis.projection();
+print('MODIS projection:', modisProjection);
+
+// Load and display forest cover data at 30 meters resolution.
+var forest = ee.Image('UMD/hansen/global_forest_change_2015')
+    .select('treecover2000');
+Map.addLayer(forest, {max: 80}, 'forest cover 30 m');
+
+// Get the forest cover data at MODIS scale and projection.
+var forestMean = forest
+    // Force the next reprojection to aggregate instead of resampling.
+    .reduceResolution({
+      reducer: ee.Reducer.mean(),
+      maxPixels: 1024
+    })
+    // Request the data at the scale and projection of the MODIS image.
+    .reproject({
+      crs: modisProjection
+    });
+
+// Display the aggregated, reprojected forest cover data.
+Map.addLayer(forestMean, {max: 80}, 'forest cover at MODIS scale');
